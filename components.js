0ae06d1e7fe8cf3d0fb9f441b6493ee6a778a05b
Tiziano Titone <titone1996@gmail.com>
2024-01-10 21:49:22 +0100

    update css

--- components.js
+++ components.js
@@ -1,5 +1,7 @@
 export const displayCards = (pics, detail, photographer) =>
-  `  <div class="card col-lg-3 col-md-3">
+  `  
+  <div class="col-4">
+  <div class="card mb-4">
 <img src= "${pics}" class="card-img-top "  alt="..." />
 <div class="card-body">
   <h5 class="card-title">${photographer}</h5>
@@ -7,5 +9,6 @@ export const displayCards = (pics, detail, photographer) =>
   ${detail}
   </p>
   <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
 </div>
 </div>`;
